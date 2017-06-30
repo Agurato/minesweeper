f0c0c3ecc74569221984c1000060f0e027b6fc9a
Agurato <mail.vmonot@gmail.com>
2017-06-30 19:01:39 +0200

    End minesweeper

--- cell.js
+++ cell.js
@@ -1,7 +1,105 @@
-function Cell() {
-    var value = 0;
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+    this.value = 0;
+    this.discovered = false;
+    this.question = false;
 }
 
-Cell.prototype.test = function() {
+Cell.prototype.count_bombs = function() {
+    if(this.value != 9) {
+        for(var off_i=-1 ; off_i<=1 ; off_i++) {
+            for(var off_j=-1 ; off_j<=1 ; off_j++) {
+                var neighbor_i = this.i + off_i;
+                var neighbor_j = this.j + off_j;
+                if(neighbor_i >= 0 && neighbor_i < cols && neighbor_j >= 0 && neighbor_j < rows) {
+                    if(grid[neighbor_i][neighbor_j].value == 9) {
+                        this.value ++;
+                    }
+                }
+            }
+        }
+    }
+};
 
+Cell.prototype.discover = function() {
+    if(! this.question) {
+        if(this.value == 9) {
+            game_over = true;
+        }
+        else {
+            if(! this.discovered) {
+                this.discovered = true;
+                nb_discovered ++;
+            }
+            if(this.value == 0) {
+                for(var off_i=-1 ; off_i<=1 ; off_i++) {
+                    for(var off_j=-1 ; off_j<=1 ; off_j++) {
+                        var neighbor_i = this.i + off_i;
+                        var neighbor_j = this.j + off_j;
+                        if(neighbor_i >= 0 && neighbor_i < cols && neighbor_j >= 0 && neighbor_j < rows) {
+                            if(grid[neighbor_i][neighbor_j].value != 9 && grid[neighbor_i][neighbor_j].discovered == false) {
+                                grid[neighbor_i][neighbor_j].discover();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+Cell.prototype.display = function() {
+    if(this.discovered) {
+        switch(this.value) {
+            case 0:
+                // Empty case
+                stroke(0);
+                fill(230);
+                rect(this.i*w, this.j*w, w, w);
+                break;
+            case 9:
+                // BOMB !!
+
+                // Draw rectangle
+                stroke(0);
+                noFill();
+                rect(this.i*w, this.j*w, w, w);
+
+                // Draw bomb
+                noStroke();
+                fill(255, 0, 0);
+                ellipseMode(CENTER);
+                ellipse(this.i*w + w/2, this.j*w + w/2, w/2);
+                noFill();
+                break;
+            default:
+                // Got bomb neighbors
+
+                // Draw rectangle
+                stroke(0);
+                fill(230);
+                rect(this.i*w, this.j*w, w, w);
+
+                // Draw value
+                fill(0);
+                textSize(20);
+                textAlign(CENTER, CENTER);
+                text(this.value, this.i*w + w/2, this.j*w + w/2);
+
+                break;
+        }
+    }
+    else {
+        stroke(0);
+        noFill();
+        rect(this.i*w, this.j*w, w, w);
+        if(this.question) {
+            noStroke();
+            fill(0, 0, 255);
+            textSize(20);
+            textAlign(CENTER, CENTER);
+            text("?", this.i*w + w/2, this.j*w + w/2);
+        }
+    }
 };
--- sketch.js
+++ sketch.js
@@ -1,51 +1,120 @@
-var cols = 30;
-var rows = 30;
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+var cols = 10;
+var rows = 10;
 var w = 30;
-var bomb_nb = 10;
-var map;
+var bomb_nb;
+var grid;
+var game_over = false;
+var nb_discovered = 0;
+var nb_questions = 0;
 
 function setup() {
-    createCanvas(cols*w+1, rows*w+1);
-    map = new Array(cols);
+    createCanvas(cols*w+200, rows*w+1);
+    grid = new Array(cols);
     for(var i=0 ; i<cols ; i++) {
-        map[i] = new Array(rows);
+        grid[i] = new Array(rows);
         for(var j=0 ; j<rows ; j++) {
-            map[i][j] = new Cell();
+            grid[i][j] = new Cell(i, j);
         }
     }
 
+    bomb_nb = floor(cols*rows/6);
     plant_bombs();
+
+    for(var i=0 ; i<cols ; i++) {
+        for(var j=0 ; j<rows ; j++) {
+            grid[i][j].count_bombs();
+        }
+    }
 }
 
 function draw() {
-    background(200);
-    textSize(20);
+    background(255);
+
+    fill(0);
+    stroke(0);
+    textAlign(LEFT, TOP);
+    textSize(16);
+    text("Bombs : " + nb_questions + " / " + bomb_nb, cols*w+30, 0);
 
     for(var i = 0 ; i<cols ; i++) {
         for(var j=0 ; j<rows ; j++) {
-            rect(i*w, j*w, w, w);
-            text("0", i*w+w/4, j*w+w/2);
+            grid[i][j].display();
         }
     }
-    // noLoop();
+
+    if(nb_discovered + bomb_nb == cols*rows) {
+        noLoop();
+
+        stroke(200);
+        fill(200);
+
+        rectMode(CENTER);
+        rect(cols*w/2, rows*w/2, 280, 70);
+        rectMode(CORNER);
+
+        stroke(40, 190, 40);
+        fill(40, 190, 40);
+        textSize(40);
+        textAlign(CENTER, CENTER);
+        text("YOU WON !", cols*w/2, rows*w/2);
+    }
+
+    if(game_over) {
+        noLoop();
+
+        for(var i = 0 ; i<cols ; i++) {
+            for(var j=0 ; j<rows ; j++) {
+                grid[i][j].discovered = true;
+                grid[i][j].display();
+            }
+        }
+
+        stroke(200);
+        fill(200);
+
+        rectMode(CENTER);
+        rect(cols*w/2, rows*w/2, 280, 70);
+        rectMode(CORNER);
+
+        stroke(255, 0, 0);
+        fill(255, 0, 0);
+        textSize(40);
+        textAlign(CENTER, CENTER);
+        text("GAME OVER", cols*w/2, rows*w/2);
+    }
 }
 
 function mousePressed() {
     i = floor(mouseX/w);
     j = floor(mouseY/w);
-    console.log(i+"; "+j);
+    if(i>=0 && i<cols && j>=0 && j<rows) {
+        // console.log(i+";"+j);
+        if (mouseButton == LEFT) {
+            grid[i][j].discover();
+        }
+        else if (mouseButton == RIGHT) {
+            if(grid[i][j].question) {
+                grid[i][j].question = false;
+                nb_questions --;
+            }
+            else {
+                grid[i][j].question = true;
+                nb_questions ++;
+            }
+        }
+    }
 }
 
 function plant_bombs() {
     var bomb_i, bomb_j;
     for(var i=0 ; i<bomb_nb ; i++) {
-        console.log(map[0][1]);
         do {
             bomb_i = floor(random(0, cols));
-            console.log(bomb_i);
             bomb_j = floor(random(0, rows));
-            console.log(bomb_j);
-        } while(map[bomb_i][bomb_j].value == 9);
-        map[bomb_i][bomb_j].value = 9;
+        } while(grid[bomb_i][bomb_j].value == 9);
+        grid[bomb_i][bomb_j].value = 9;
+        // console.log(grid[bomb_i][bomb_j]);
     }
 }
